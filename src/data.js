d3ca536c406fe0ea3ce9667d66de0c99092b643e
sophi-li <sophiali124@gmail.com>
2022-04-10 11:28:16 -0700

    add places

--- src/data.js
+++ src/data.js
@@ -20,7 +20,7 @@ const data = [
   {
     name: "Golden Flower Vietnamese Restaurant",
     city: "San Francisco",
-    neighborhood: "Mission",
+    neighborhood: "Chinatown",
     items: [
       {
         item: "#1 Pho (rare slices of steak, well-done brisket, flank, tandon, tripe & beef balls)",
@@ -56,16 +56,38 @@ const data = [
     ],
   },
   {
+    name: "Golden Boy Pizza",
+    city: "San Francisco",
+    neighborhood: "North Beach",
+    items: [{ item: "Garlic and Clam Pizza", isVeg: false }],
+    contact: [
+      {
+        website: "http://www.goldenboypizza.com/",
+      },
+    ],
+  },
+  {
+    name: "Stella Pastry and Cafe",
+    city: "San Francisco",
+    neighborhood: "North Beach",
+    items: [{ item: "Tiramisu", isVeg: false }],
+    contact: [
+      {
+        directions: "https://www.google.com/maps/dir//Stella+Pastry,+Columbus+Avenue,+San+Francisco,+CA/@37.7638585,-122.5022574,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x80858142bbda3f7d:0x4e12e74cf9c2346c!2m2!1d-122.4083382!2d37.799169",
+      },
+    ],
+  },
+  {
     name: "San Tung",
     city: "San Francisco",
-    neighborhood: "Sunset",
+    neighborhood: "Inner Sunset",
     items: [{ item: "Dry Fried Chicken Wings", isVeg: false }],
     contact: [{ phone: "415-242-0828", website: "http://www.santung.net/" }],
   },
   {
     name: "Marugame Udon",
     city: "San Francisco",
-    neighborhood: "Sunset",
+    neighborhood: "Outer Sunet",
     items: [
       { item: "Nikutama", isVeg: false },
       { item: "Mentai Kamatama", isVeg: false },
@@ -81,9 +103,20 @@ const data = [
     ],
   },
   {
+    name: "Tín Vietnamese Cuisine ",
+    city: "San Francisco",
+    neighborhood: "SoMa",
+    items: [{ item: "Pho", isVeg: false }],
+    contact: [
+      {
+        website: "https://www.tinsf.com/",
+      },
+    ],
+  },
+  {
     name: "Kevin’s Noodle House",
     city: "San Francisco",
-    neighborhood: "Sunset",
+    neighborhood: "Outer Sunset",
     items: [
       {
         item: "#1 Đặc Biệt Xe Lửa (Special combo w/ thin slice of steak, well-done brisket, flank, tendon & tripe)",
@@ -103,7 +136,7 @@ const data = [
   {
     name: "Burma Superstar",
     city: "San Francisco",
-    neighborhood: "Sunset",
+    neighborhood: "Richmond",
     items: [
       { item: "Tea Leaf Salad", isVeg: true },
       { item: "Platha and Dip", isVeg: false },
@@ -113,6 +146,107 @@ const data = [
         phone: "415-387-2147",
         website: "https://www.burmasuperstar.com/",
         order: "https://direct.chownow.com/order/20280/locations/",
+      },
+    ],
+  },
+  {
+    name: "Han Il Kwan",
+    city: "San Francisco",
+    neighborhood: "Richmond",
+    items: [
+      { item: "Beef Tofu Soup", isVeg: true },
+    ],
+    contact: [
+      {
+        directions: "https://www.google.com/maps/dir//Han+Il+Kwan,+1802+Balboa+St,+San+Francisco,+CA+94121/@37.7767645,-122.5133073,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x80858712366ca223:0x33fbb2cde7057379!2m2!1d-122.4782853!2d37.7767113",
+      },
+    ],
+  },
+  {
+    name: "Manna",
+    city: "San Francisco",
+    neighborhood: "Inner Sunset",
+    items: [
+      { item: "Beef Tofu Soup", isVeg: true },
+    ],
+    contact: [
+      {
+        directions: "https://www.google.com/maps/dir//Manna,+Irving+Street,+San+Francisco,+CA/@37.7767645,-122.5133073,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8085875db0172dc1:0xaed20b939085cf6a!2m2!1d-122.4672514!2d37.7638913",
+      },
+    ],
+  },
+  {
+    name: "Taquería El Farlito",
+    city: "San Francisco",
+    neighborhood: "Mission",
+    items: [{ item: "Super Burrito", isVeg: false }],
+    contact: [
+      {
+        website: "https://elfarolitosf.com/",
+        directions:
+          "https://www.google.com/maps/dir//Taqueria+El+Farolito,+24th+Street,+SF,+CA/@37.7528373,-122.4461775,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x808f7e48f251180f:0x4d563cdb72ff9be!2m2!1d-122.4111439!2d37.7527847",
+      },
+    ],
+  },
+  {
+    name: "Panchita’s Pupuseria & Restaurant",
+    city: "San Francisco",
+    neighborhood: "Mission",
+    items: [
+      { item: "Pork Pupusa", isVeg: false },
+      { item: "Pork + Cheese  Pupusa", isVeg: false },
+    ],
+    contact: [
+      {
+        website: "https://www.panchitaspupuseria.com/",
+        directions:
+          "https://www.google.com/maps/dir//Panchita's+Pupuser%C3%ADa+%26+Restaurant,+16th+Street,+San+Francisco,+CA/@37.7647848,-122.4566431,13z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x808f7e2251f715f7:0x542a4fc2580e66c!2m2!1d-122.4216237!2d37.76479",
+        order:
+          "https://www.clover.com/online-ordering/panchitas-restaurant-2-san-francisco/508SZP8BYPCKP",
+      },
+    ],
+  },
+  {
+    name: "Andytown Coffee Roasters",
+    city: "San Francisco",
+    neighborhood: "Outer Sunset",
+    items: [
+      { item: "Snowy Plover", isVeg: false },
+    ],
+    contact: [
+      {
+        website: "https://www.andytownsf.com/",
+        order: "https://www.andytownsf.com/online-cafe-orders",
+      },
+    ],
+  },
+  {
+    name: "Just For You Cafe",
+    city: "San Francisco",
+    neighborhood: "Dogpatch",
+    items: [
+      { item: "Crabby Bennie", isVeg: false },
+      { item: "Beignets", isVeg: false },
+    ],
+    contact: [
+      {
+        website: "https://www.justforyoucafe.com/",
+        phone: "415-647-3303"
+      },
+    ],
+  },
+  {
+    name: "Daily Driver",
+    city: "San Francisco",
+    neighborhood: "Dogpatch",
+    items: [
+      { item: "Bagel + Cultured Butter", isVeg: false },
+      { item: "Bagel + Gravlox", isVeg: false },
+    ],
+    contact: [
+      {
+        website: "https://dailydriver.com/",
+        order: "https://dailydriver.com/order/"
       },
     ],
   },
@@ -131,6 +265,28 @@ const data = [
       {
         website: "https://www.neighborsf.com/",
         order: "https://direct.chownow.com/order/28481/locations/42152",
+      },
+    ],
+  },
+  {
+    name: "Barebottle Brewing Company",
+    city: "San Francisco",
+    neighborhood: "Bernal Heights",
+    items: [],
+    contact: [
+      {
+        website: "https://www.barebottle.com/",
+      },
+    ],
+  },
+  {
+    name: "Southern Pacific Brewing",
+    city: "San Francisco",
+    neighborhood: "Mission",
+    items: [],
+    contact: [
+      {
+        website: "https://www.southernpacificbrewing.com/",
       },
     ],
   },
@@ -209,6 +365,30 @@ const data = [
       {
         directions:
           "https://www.google.com/maps/dir//tasty+dumpling+nyc/@33.8884054,-134.3335494,3z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89c25a26dfa3a383:0x65e91ab05b8238c0!2m2!1d-73.9995503!2d40.7149266",
+      },
+    ],
+  },
+  {
+    name: "Wah Fung No. 1",
+    city: "Manhattan",
+    neighborhood: "Chinatown",
+    items: [{ item: "Cha Sui + Rice", isVeg: false }],
+    contact: [
+      {
+        directions:
+          "https://www.google.com/maps/dir//Wah+Fung+Fast+Food,+Chrystie+Street,+New+York,+NY/@40.7173136,-74.0294843,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89c25987ffa99e09:0xfbce3ee954c0d58a!2m2!1d-73.9944521!2d40.7173132",
+      },
+    ],
+  },
+  {
+    name: "Congee Village",
+    city: "Manhattan",
+    neighborhood: "Chinatown",
+    items: [{ item: "Congee", isVeg: false }],
+    contact: [
+      {
+        directions:
+          "https://www.google.com/maps/dir//Congee+Village,+Allen+Street,+New+York,+NY/@40.7186537,-74.0253734,13z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89c25987185dccc7:0x337115f296625e4f!2m2!1d-73.990354!2d40.718659",
       },
     ],
   },
@@ -363,15 +543,24 @@ const data = [
     ],
   },
   {
-    name: "7th Street Burger",
+    name: "Dominique Ansel",
     city: "Manhattan",
-    neighborhood: "East Village",
-    items: [{ item: "Cheeseburger", isVeg: false }],
+    neighborhood: "Soho",
+    items: [{ item: "Brown Sugar DKA", isVeg: false }],
     contact: [
       {
-        order:
-          "https://www.toasttab.com/7th-street-burger-91-e-7th-st/v3/?mode=fulfillment",
-        website: "https://www.7thstreetburger.com/",
+        website: "https://www.dominiqueansel.com/",
+      },
+    ],
+  },
+  {
+    name: "Kona Coffee",
+    city: "Manhattan",
+    neighborhood: "East Village",
+    items: [{ item: "Hawaiian Latte", isVeg: false }],
+    contact: [
+      {
+        website: "https://www.konacoffee.nyc/",
       },
     ],
   },
@@ -384,6 +573,19 @@ const data = [
       {
         website: "https://www.abraconyc.com/",
         order: "https://www.abraconyc.com/s/order",
+      },
+    ],
+  },
+  {
+    name: "7th Street Burger",
+    city: "Manhattan",
+    neighborhood: "East Village",
+    items: [{ item: "Cheeseburger", isVeg: false }],
+    contact: [
+      {
+        order:
+          "https://www.toasttab.com/7th-street-burger-91-e-7th-st/v3/?mode=fulfillment",
+        website: "https://www.7thstreetburger.com/",
       },
     ],
   },
@@ -528,6 +730,21 @@ const data = [
     contact: [
       {
         website: "https://www.broderpdx.com/",
+      },
+    ],
+  },
+  {
+    name: "Jen’s Pastries",
+    city: "Portland",
+    neighborhood: "South Portland",
+    items: [
+      { item: "Berry Poptart", isVeg: false },
+      { item: "Maple Oat Scone", isVeg: false },
+      { item: "Garlic Croissant", isVeg: false },
+    ],
+    contact: [
+      {
+        website: "https://www.jenspdx.com/",
       },
     ],
   },
